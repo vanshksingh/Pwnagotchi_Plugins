fccacf119c1bc78dc916f186db35bf25aa18951e
Vansh Kumar Singh <114809624+vanshksingh@users.noreply.github.com>
2024-06-28 02:25:28 +0530

    Update epdtest.py

--- epdtest.py
+++ epdtest.py
@@ -1,19 +1,19 @@
-# *****************************************************************************
-# * | File        :	  epd2in13b_V4.py
+# /*****************************************************************************
+# * | File        :	  epdconfig.py
 # * | Author      :   Waveshare team
-# * | Function    :   Electronic paper driver
+# * | Function    :   Hardware underlying interface
 # * | Info        :
 # *----------------
-# * | This version:   V1.0
-# * | Date        :   2022-04-21
-# # | Info        :   python demo
-# -----------------------------------------------------------------------------
+# * | This version:   V1.2
+# * | Date        :   2022-10-29
+# * | Info        :
+# ******************************************************************************
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
+# of this software and associated documnetation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to  whom the Software is
-# furnished to do so, subject to the following conditions:
+# furished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
@@ -27,178 +27,141 @@
 # THE SOFTWARE.
 #
 
+import os
 import logging
-from pwnagotchi.ui.hw.libs.waveshare.epaper import epdconfig
+import sys
+import time
+import subprocess
 
-# Display resolution
-EPD_WIDTH = 122
-EPD_HEIGHT = 250
+from ctypes import *
 
 logger = logging.getLogger(__name__)
 
 
-class EPD:
+class RaspberryPi:
+    # Pin definition
+    RST_PIN = 17
+    DC_PIN = 25
+    CS_PIN = 8
+    BUSY_PIN = 24
+    PWR_PIN = 18
+    MOSI_PIN = 10
+    SCLK_PIN = 11
+
     def __init__(self):
-        self.reset_pin = epdconfig.RST_PIN
-        self.dc_pin = epdconfig.DC_PIN
-        self.busy_pin = epdconfig.BUSY_PIN
-        self.cs_pin = epdconfig.CS_PIN
-        self.width = EPD_WIDTH
-        self.height = EPD_HEIGHT
+        import spidev
+        import gpiozero
 
-    # hardware reset
-    def reset(self):
-        epdconfig.digital_write(self.reset_pin, 1)
-        epdconfig.delay_ms(20)
-        epdconfig.digital_write(self.reset_pin, 0)
-        epdconfig.delay_ms(2)
-        epdconfig.digital_write(self.reset_pin, 1)
-        epdconfig.delay_ms(20)
+        self.SPI = spidev.SpiDev(0, 0)
+        self.GPIO_RST_PIN = gpiozero.LED(self.RST_PIN)
+        self.GPIO_DC_PIN = gpiozero.LED(self.DC_PIN)
+        # self.GPIO_CS_PIN = gpiozero.LED(self.CS_PIN)
+        self.GPIO_PWR_PIN = gpiozero.LED(self.PWR_PIN)
+        self.GPIO_BUSY_PIN = gpiozero.Button(self.BUSY_PIN, pull_up=False)
 
-    # send 1 byte command
-    def send_command(self, command):
-        epdconfig.digital_write(self.dc_pin, 0)
-        epdconfig.digital_write(self.cs_pin, 0)
-        epdconfig.spi_writebyte([command])
-        epdconfig.digital_write(self.cs_pin, 1)
+    def digital_write(self, pin, value):
+        if pin == self.RST_PIN:
+            if value:
+                self.GPIO_RST_PIN.on()
+            else:
+                self.GPIO_RST_PIN.off()
+        elif pin == self.DC_PIN:
+            if value:
+                self.GPIO_DC_PIN.on()
+            else:
+                self.GPIO_DC_PIN.off()
+        # elif pin == self.CS_PIN:
+        #    if value:
+        #        self.GPIO_CS_PIN.on()
+        #    else:
+        #        self.GPIO_CS_PIN.off()
+        elif pin == self.PWR_PIN:
+            if value:
+                self.GPIO_PWR_PIN.on()
+            else:
+                self.GPIO_PWR_PIN.off()
 
-    # send 1 byte data
-    def send_data(self, data):
-        epdconfig.digital_write(self.dc_pin, 1)
-        epdconfig.digital_write(self.cs_pin, 0)
-        epdconfig.spi_writebyte([data])
-        epdconfig.digital_write(self.cs_pin, 1)
+    def digital_read(self, pin):
+        if pin == self.BUSY_PIN:
+            return self.GPIO_BUSY_PIN.value
+        elif pin == self.RST_PIN:
+            return self.RST_PIN.value
+        elif pin == self.DC_PIN:
+            return self.DC_PIN.value
+        # elif pin == self.CS_PIN:
+        #     return self.CS_PIN.value
+        elif pin == self.PWR_PIN:
+            return self.PWR_PIN.value
 
-    # send a lot of data
-    def send_data2(self, data):
-        epdconfig.digital_write(self.dc_pin, 1)
-        epdconfig.digital_write(self.cs_pin, 0)
-        epdconfig.spi_writebyte2(data)
-        epdconfig.digital_write(self.cs_pin, 1)
+    def delay_ms(self, delaytime):
+        time.sleep(delaytime / 1000.0)
 
-    # judge e-Paper whether is busy
-    def busy(self):
-        logger.debug("e-Paper busy")
-        while (epdconfig.digital_read(self.busy_pin) != 0):
-            epdconfig.delay_ms(10)
-        logger.debug("e-Paper busy release")
+    def spi_writebyte(self, data):
+        self.SPI.writebytes(data)
 
-    # set the display window
-    def set_windows(self, xstart, ystart, xend, yend):
-        self.send_command(0x44)  # SET_RAM_X_ADDRESS_START_END_POSITION
-        self.send_data((xstart >> 3) & 0xff)
-        self.send_data((xend >> 3) & 0xff)
+    def spi_writebyte2(self, data):
+        self.SPI.writebytes2(data)
 
-        self.send_command(0x45)  # SET_RAM_Y_ADDRESS_START_END_POSITION
-        self.send_data(ystart & 0xff)
-        self.send_data((ystart >> 8) & 0xff)
-        self.send_data(yend & 0xff)
-        self.send_data((yend >> 8) & 0xff)
+    def DEV_SPI_write(self, data):
+        self.DEV_SPI.DEV_SPI_SendData(data)
 
-    # set the display cursor(origin)
-    def set_cursor(self, xstart, ystart):
-        self.send_command(0x4E)  # SET_RAM_X_ADDRESS_COUNTER
-        self.send_data(xstart & 0xff)
+    def DEV_SPI_nwrite(self, data):
+        self.DEV_SPI.DEV_SPI_SendnData(data)
 
-        self.send_command(0x4F)  # SET_RAM_Y_ADDRESS_COUNTER
-        self.send_data(ystart & 0xff)
-        self.send_data((ystart >> 8) & 0xff)
+    def DEV_SPI_read(self):
+        return self.DEV_SPI.DEV_SPI_ReadData()
 
-    # initialize
-    def init(self):
-        if (epdconfig.module_init() != 0):
-            return -1
+    def module_init(self, cleanup=False):
+        self.GPIO_PWR_PIN.on()
 
-        self.reset()
+        if cleanup:
+            find_dirs = [
+                os.path.dirname(os.path.realpath(__file__)),
+                '/usr/local/lib',
+                '/usr/lib',
+            ]
+            self.DEV_SPI = None
+            for find_dir in find_dirs:
+                val = int(os.popen('getconf LONG_BIT').read())
+                logging.debug("System is %d bit" % val)
+                if val == 64:
+                    so_filename = os.path.join(find_dir, 'DEV_Config_64.so')
+                else:
+                    so_filename = os.path.join(find_dir, 'DEV_Config_32.so')
+                if os.path.exists(so_filename):
+                    self.DEV_SPI = CDLL(so_filename)
+                    break
+            if self.DEV_SPI is None:
+                RuntimeError('Cannot find DEV_Config.so')
 
-        self.busy()
-        self.send_command(0x12)  # SWRESET
-        self.busy()
+            self.DEV_SPI.DEV_Module_Init()
 
-        self.send_command(0x01)  # Driver output control
-        self.send_data(0xf9)
-        self.send_data(0x00)
-        self.send_data(0x00)
-
-        self.send_command(0x11)  # data entry mode
-        self.send_data(0x03)
-
-        self.set_windows(0, 0, self.width - 1, self.height - 1)
-        self.set_cursor(0, 0)
-
-        self.send_command(0x3C)  # BorderWavefrom
-        self.send_data(0x05)
-
-        self.send_command(0x18)  # Read built-in temperature sensor
-        self.send_data(0x80)
-
-        self.send_command(0x21)  # Display update control
-        self.send_data(0x80)
-        self.send_data(0x80)
-
-        self.busy()
-
+        else:
+            # SPI device, bus = 0, device = 0
+            self.SPI.open(0, 0)
+            self.SPI.max_speed_hz = 4000000
+            self.SPI.mode = 0b00
         return 0
 
-    # turn on display
-    def ondisplay(self):
-        self.send_command(0x20)
-        self.busy()
+    def module_exit(self, cleanup=False):
+        logger.debug("spi end")
+        self.SPI.close()
 
-    # image converted to bytearray
-    def getbuffer(self, image):
-        img = image
-        imwidth, imheight = img.size
-        if (imwidth == self.width and imheight == self.height):
-            img = img.convert('1')
-        elif (imwidth == self.height and imheight == self.width):
-            # image has correct dimensions, but needs to be rotated
-            img = img.rotate(90, expand=True).convert('1')
-        else:
-            logger.warning("Wrong image dimensions: must be " +
-                           str(self.width) + "x" + str(self.height))
-            # return a blank buffer
-            return [0x00] * (int(self.width/8) * self.height)
+        self.GPIO_RST_PIN.off()
+        self.GPIO_DC_PIN.off()
+        # self.GPIO_CS_PIN.off()
+        self.GPIO_PWR_PIN.off()
+        logger.debug("close 5V, Module enters 0 power consumption ...")
 
-        buf = bytearray(img.tobytes('raw'))
-        return buf
+        if cleanup:
+            self.GPIO_RST_PIN.close()
+            self.GPIO_DC_PIN.close()
+            # self.GPIO_CS_PIN.close()
+            self.GPIO_PWR_PIN.close()
+            self.GPIO_BUSY_PIN.close()
 
-    # display image
-    def display(self, imageblack, imagered):
-        self.send_command(0x24)
-        self.send_data2(imageblack)
 
-        self.send_command(0x26)
-        self.send_data2(imagered)
+implementation = RaspberryPi()
 
-        self.ondisplay()
-
-    # display white image
-    def clear(self):
-        if self.width % 8 == 0:
-            linewidth = int(self.width/8)
-        else:
-            linewidth = int(self.width/8) + 1
-
-        buf = [0xff] * (int(linewidth * self.height))
-
-        self.send_command(0x24)
-        self.send_data2(buf)
-
-        self.send_command(0x26)
-        self.send_data2(buf)
-
-        self.ondisplay()
-
-    # Compatible with older version functions
-    def Clear(self):
-        self.clear()
-
-    # sleep
-    def sleep(self):
-        self.send_command(0x10)  # DEEP_SLEEP
-        self.send_data(0x01)  # check code
-
-        epdconfig.delay_ms(2000)
-        epdconfig.module_exit()
-### END OF FILE ###
+for func in [x for x in dir(implementation) if not x.startswith('_')]:
+    setattr(sys.modules[__name__], func, getattr(implementation, func))
